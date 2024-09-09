b8c5920ffeb9e566714c37a98e9bae7052570f5f
SiddharajYadav <sidharajyadav157@gmail.com>
2024-09-09 12:21:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@
 
 // app.listen(port, () => {
 //   console.log(`Example app listening on port ${port}`)
-// })
+// }) 
 const express = require('express');
 const errorHandler = require('./middleware/errorHandler');
 const dotenv = require('dotenv').config();
